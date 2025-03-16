e7406a49f7da966fc846b40f6a271d044101fcfb
ChoriyevJamshid <qpq777111000@gmail.com>
2025-03-16 14:18:28 +0500

    feat: created src directory and moved fastapi depends files and packages into src dir.

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -1,6 +1,5 @@
 __all__ = (
     "settings",
-    "db",
     "create_superuser",
     "Base",
     "User",
@@ -10,6 +9,5 @@ __all__ = (
 )
 
 from .config import settings
-from .db_connector import db
 from .models import User, Patient, Hospitalization, MedicalRecords, Base
 from .create_superuser import create_superuser
