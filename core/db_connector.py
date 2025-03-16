d1a8e741c4fc1bfb5a7eaa25d3e11930d7fc4e6d
ChoriyevJamshid <qpq777111000@gmail.com>
2025-03-16 14:08:00 +0500

    feat: add db/db.py

--- core/db_connector.py
+++ core/db_connector.py
@@ -1,32 +1,3 @@
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
-from core.config import settings
-
-
-class DBConnector:
-    def __init__(self, url: str, echo: bool = False):
-
-        self.engine = create_async_engine(
-            url=url,
-            echo=echo,
-        )
-        self.session_factory = async_sessionmaker(
-            bind=self.engine,
-            autocommit=False,
-            expire_on_commit=False,
-            autoflush=False,
-        )
-
-
-    async def generate_session(self):
-        async with self.session_factory() as session:
-            yield session
-            await session.close()
-
-
-db = DBConnector(
-    url=settings.db.url,
-    echo=settings.db.echo,
-)
 
 
 
