824af2cfb63b951ee0c2aaaf7a2a7067a2365eb7
ChoriyevJamshid <qpq777111000@gmail.com>
2025-04-15 17:32:03 +0500

    feat: connect with faeger, updated docker-compose.yml and requirements.text

--- src/app.py
+++ src/app.py
@@ -5,6 +5,30 @@ from fastapi import FastAPI
 from src.api import router as api_router
 from src.core.create_superuser import create_superuser
 
+from opentelemetry import trace
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+from opentelemetry.exporter.jaeger.thrift import JaegerExporter
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+
+
+jaeger_exporter = JaegerExporter(
+    agent_host_name="jaeger",  # имя контейнера из docker-compose
+    agent_port=6831,
+)
+
+# Настройка провайдера и процессора
+trace.set_tracer_provider(
+    TracerProvider(
+        resource=Resource.create({SERVICE_NAME: "fastapi-service"})
+    )
+)
+trace.get_tracer_provider().add_span_processor(
+    BatchSpanProcessor(jaeger_exporter)
+)
+
+
 @asynccontextmanager
 async def lifespan(main_app: FastAPI):
     try:
@@ -16,6 +40,7 @@ async def lifespan(main_app: FastAPI):
 app = FastAPI(
     lifespan=lifespan,
 )
+FastAPIInstrumentor.instrument_app(app)
 app.include_router(api_router)
 
 
